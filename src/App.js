78ea2064598cd90250371146fdd0b858c97f393c
Abigail Collins <abigailjcollins@gmail.com>
2024-03-01 09:41:24 -0500

    first push

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+
+export default class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+		  count: 0
+		};
+		this.increment = this.increment.bind(this);
+		this.decrement = this.decrement.bind(this);
+		this.reset = this.reset.bind(this);
+	  
+	  }
+	  
+	 increment(){
+	  this.setState(state =>({
+		count: state.count + 1
+	  }))
+	 }
+	 decrement(){
+	  this.setState(state =>({
+		count: state.count - 1
+	  }))
+	 }
+	 reset(){
+	  this.setState({
+		count: 0
+	  })
+	 }
+  
+	  render() {
+		return (
+		  <div>
+			<button className='inc' onClick={this.increment}>+1</button>
+			<button className='dec' onClick={this.decrement}>-1</button>
+			<button className='reset' onClick={this.reset}>Reset</button>
+			<h1>Current Count: {this.state.count}</h1>
+
+		  </div>
+		);
+	  }
+	};
+
+
+
